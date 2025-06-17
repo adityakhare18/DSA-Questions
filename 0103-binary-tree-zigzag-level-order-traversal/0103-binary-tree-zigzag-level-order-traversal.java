ef4990ba8d1b6c7b88fcf54b0239c23bb34b84e5
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-06-17 14:42:40 +0530

    Time: 1 ms (66.14%), Space: 42.5 MB (24.63%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,42 +15,31 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
         if(root == null)
-        return ans;
+        return new ArrayList<>();
 
-        Deque<TreeNode> dq = new ArrayDeque<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        dq.add(root);
+        q.add(root);
         int level = 1;
-        while(!dq.isEmpty()){
+        while(!q.isEmpty()){
+            int size = q.size();
             List<Integer> ls = new ArrayList<>();
-            if(level % 2 != 0){
-                int size = dq.size();
-                for(int i=0;i<size;i++){
-                    TreeNode curr = dq.removeFirst();
-                    ls.add(curr.val);
-
-                    if(curr.left != null)
-                    dq.addLast(curr.left);
-                    if(curr.right != null)
-                    dq.addLast(curr.right);
-                }
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.remove();
+                ls.add(curr.val);
+                if(curr.left != null)
+                q.add(curr.left);
+                if(curr.right != null)
+                q.add(curr.right);
             }
-            else{
-                int size = dq.size();
-                for(int i=0;i<size;i++){
-                    TreeNode curr = dq.removeLast();
-                    ls.add(curr.val);
-
-                    if(curr.right != null)
-                    dq.addFirst(curr.right);
-                    if(curr.left != null)
-                    dq.addFirst(curr.left);
-                }
+            if ((level & 1) == 0) {
+                Collections.reverse(ls); 
             }
+
+            ans.add(ls);  
             level++;
-            ans.add(ls);
         }
         return ans;
     }
