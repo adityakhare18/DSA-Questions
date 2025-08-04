f2d155ecdd85f8e525b871a1e57ec861bd4efa43
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-08-04 07:09:12 +0530

    Time: 1 ms (62.15%), Space: 45.8 MB (93.54%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-
-        
-        int[] left = findLeft(height,n);
-
-        int[] right = findRight(height,n);
+    public int trap(int[] arr) {
+        int n = arr.length;
+        int[] left = findLeft(arr,n);
+        int[] right = findRight(arr,n);
 
         int total = 0;
-
         for(int i=0;i<n;i++){
-            total += (Math.min(left[i],right[i]) - height[i]);
+            total += (Math.min(left[i],right[i])) - arr[i];
         }
         return total;
+
     }
+
     public int[] findLeft(int[] arr,int n){
         int[] left = new int[n];
-
         left[0] = arr[0];
         for(int i=1;i<n;i++){
             left[i] = Math.max(left[i-1],arr[i]);
@@ -25,7 +22,6 @@ class Solution {
     }
     public int[] findRight(int[] arr,int n){
         int[] right = new int[n];
-
         right[n-1] = arr[n-1];
         for(int i=n-2;i>=0;i--){
             right[i] = Math.max(right[i+1],arr[i]);
