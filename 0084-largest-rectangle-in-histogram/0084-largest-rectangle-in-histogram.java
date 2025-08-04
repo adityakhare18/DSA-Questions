ffdef9a1f85cf256dc9e054b80981c2aec452d20
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-08-04 07:28:26 +0530

    Time: 118 ms (9.67%), Space: 57.2 MB (80.16%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,47 +1,44 @@
 class Solution {
     public int largestRectangleArea(int[] arr) {
         int n = arr.length;
-        int[] prev = prevSmaller(arr,n);
         int[] next = nextSmaller(arr,n);
-
+        int[] prev = prevSmaller(arr,n);
         int maxArea = Integer.MIN_VALUE;
 
         for(int i=0;i<n;i++){
             int l = arr[i];
+
             if(next[i] == -1)
             next[i] = n;
 
-            int b = next[i] - prev[i] -1 ;
+            int b = next[i] - prev[i] - 1;
 
             maxArea = Math.max(maxArea,l*b);
         }
         return maxArea;
     }
-    public int[] prevSmaller(int[] arr,int n){
-        Deque<Integer> dq = new ArrayDeque<>();
-
-        int[] ans = new int[n];
-        for(int i=0;i<n;i++){
-            while(!dq.isEmpty() && arr[dq.peek()] >= arr[i])
-            dq.pop();
-
-            ans[i] = dq.size() == 0 ? -1 : dq.peek();
-            dq.push(i);
-        }
-        return ans;
-    }
-
     public int[] nextSmaller(int[] arr,int n){
-        Deque<Integer> dq = new ArrayDeque<>();
-        int[] ans = new int[n];
-
+        Stack<Integer> st = new Stack<>();
+        int[] next = new int[n];
         for(int i=n-1;i>=0;i--){
-            while(!dq.isEmpty() && arr[dq.peek()] >= arr[i])
-            dq.pop();
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
+            st.pop();
 
-            ans[i] = dq.isEmpty() ? -1 : dq.peek();
-            dq.push(i);
+            next[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
-        return ans;
+        return next;
+    }
+    public int[] prevSmaller(int[] arr,int n){
+        Stack<Integer> st = new Stack<>();
+        int[] prev = new int[n];
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
+            st.pop();
+
+            prev[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return prev;
     }
 }
