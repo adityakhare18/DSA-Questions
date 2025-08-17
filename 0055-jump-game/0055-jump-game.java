19e8d1038edfd347723eba89de3d5ffc94e89002
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-08-17 06:44:03 +0530

    Time: 2 ms (83.49%), Space: 45.7 MB (71.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,10 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int maxIdx = 0;
+    public boolean canJump(int[] arr) {
+        int n = arr.length;
+        int maxIdx = 0;//how much idx frog can jump.
         for(int i=0;i<n;i++){
             if(i > maxIdx) return false;
-            maxIdx = Math.max(maxIdx,i+nums[i]);
+            maxIdx = Math.max(maxIdx,i+arr[i]);
         }
         return true;
     }
