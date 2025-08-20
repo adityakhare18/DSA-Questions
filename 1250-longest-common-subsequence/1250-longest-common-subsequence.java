0f999cff793ff5722a28b883ec725688115655bb
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-08-20 07:37:03 +0530

    Time: 46 ms (5.17%), Space: 50.6 MB (86.49%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,27 +1,23 @@
 class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        return solve(s1, s2, dp);
+
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for(int[] r:dp)
+        Arrays.fill(r,-1);        
+        return solve(s1,s2,0,0,dp);
     }
+    public int solve(String s1,String s2,int i,int j,int[][] dp){
+        if(i >= s1.length() || j >= s2.length())
+        return 0;
 
-    public int solve(String s1, String s2, int[][] dp) {
-        int n = s1.length();
-        int m = s2.length();
+        if(dp[i][j] != -1)
+        return dp[i][j];
 
-        // Initialization: First row and column are already 0 by default (Java arrays)
+        int notMatch = Math.max(solve(s1,s2,i+1,j,dp),solve(s1,s2,i,j+1,dp));
+        int match = 0;
+        if(s1.charAt(i) == s2.charAt(j))
+        match = 1 + solve(s1,s2,i+1,j+1,dp);
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-
-        return dp[n][m];
+        return dp[i][j] = Math.max(match,notMatch);
     }
 }
