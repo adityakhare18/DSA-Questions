2829329cf4ba31e810171e35a5b90f056068569d
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-08-20 07:12:41 +0530

    Time: 0 ms (100%), Space: 41.1 MB (64.39%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,34 +1,26 @@
 class Solution {
     public int rob(int[] arr) {
-        int n = arr.length;
+        if(arr.length == 1)
+        return arr[0];
+        int[] ignoreFirst = Arrays.copyOfRange(arr,1,arr.length);
+        int[] ignoreLast = Arrays.copyOfRange(arr,0,arr.length-1);
+        int[] dp1 = new int[arr.length];
+        Arrays.fill(dp1,-1);
 
-        if (n == 1)
-            return arr[0];
-
-        // Create two subarrays
-        int[] noFirst = Arrays.copyOfRange(arr, 1, n);  // excludes 0th
-        int[] noLast = Arrays.copyOfRange(arr, 0, n - 1); // excludes last
-
-        return Math.max(
-            solve(noFirst, noFirst.length - 1, new int[noFirst.length]),
-            solve(noLast, noLast.length - 1, new int[noLast.length])
-        );
+        int[] dp2 = new int[arr.length];
+        Arrays.fill(dp2,-1);
+        return Math.max(solve(ignoreFirst,0,dp1),solve(ignoreLast,0,dp2));
     }
-    public int solve(int[] arr, int idx, int[] dp) {
-        Arrays.fill(dp, -1);
-        return helper(arr, idx, dp);
-    }
-    public int helper(int[] arr,int idx,int[] dp){
-        if (idx == 0)
-            return arr[0];
-        if (idx < 0)
-            return 0;
-        if (dp[idx] != -1)
-            return dp[idx];
+    public int solve(int[] arr,int idx,int[] dp){
+        if(idx >= arr.length)
+        return 0;
 
-        int pick = arr[idx] + helper(arr, idx - 2, dp);
-        int notPick = helper(arr, idx - 1, dp);
+        if(dp[idx] != -1)
+        return dp[idx];
 
-        return dp[idx] = Math.max(pick, notPick);
+        int pick = arr[idx] + solve(arr,idx+2,dp);
+        int notPick = solve(arr,idx+1,dp);
+
+        return dp[idx] = Math.max(pick,notPick);
     }
 }
