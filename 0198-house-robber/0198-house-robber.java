de262c7780c387585751a74d788437dd11c500b0
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-08-20 07:05:23 +0530

    Time: 0 ms (100%), Space: 40.7 MB (96.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,9 +2,9 @@ class Solution {
     public int rob(int[] arr) {
         int[] dp = new int[arr.length+1];
         Arrays.fill(dp,-1);
-        return solve(arr,dp,0);
+        return solve(arr,0,dp);
     }
-    public int solve(int[] arr,int[] dp,int idx){
+    public int solve(int[] arr,int idx,int[] dp){
         if(idx >= arr.length){
             return 0;
         }
@@ -12,11 +12,10 @@ class Solution {
         if(dp[idx] != -1)
         return dp[idx];
 
-        //exclude
-        int noTake = solve(arr,dp,idx+1);
 
-        int take = arr[idx] + solve(arr,dp,idx+2);
+        int pick = arr[idx] + solve(arr,idx+2,dp);
+        int notPick = solve(arr,idx+1,dp);
 
-        return dp[idx] = Math.max(take,noTake);
+        return dp[idx] = Math.max(pick,notPick);
     }
 }
