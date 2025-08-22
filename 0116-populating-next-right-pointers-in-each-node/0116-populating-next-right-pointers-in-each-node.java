5ec7efe53792d8f1adbb89ed0fdac65683c80485
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-08-22 08:10:23 +0530

    Time: 3 ms (33.42%), Space: 44.2 MB (93.25%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,28 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
 class Solution {
     public Node connect(Node root) {
-        if (root == null)
-            return null;
-
+        if(root == null)
+        return root;
         Queue<Node> q = new LinkedList<>();
         q.add(root);
-
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int size = q.size();
+            for(int i=0;i<size;i++){
+                Node curr = q.poll();
+                if(i == size-1)
+                curr.next = null;
+                else 
+                curr.next = q.peek();
 
-            for (int i = 0; i < size; i++) {
-                Node curr = q.remove();
-
-                // If it's not the last node in the level
-                if (i < size - 1) {
-                    curr.next = q.peek();  // next node in the queue is the next right node
-                } else {
-                    curr.next = null;  // last node in this level
-                }
-
-                if (curr.left != null)
-                    q.add(curr.left);
-                if (curr.right != null)
-                    q.add(curr.right);
+                if(curr.left != null)
+                q.add(curr.left);
+                if(curr.right != null)
+                q.add(curr.right);
             }
         }
         return root;
