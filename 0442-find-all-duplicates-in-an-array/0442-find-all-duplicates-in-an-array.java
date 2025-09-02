d4baae617de8a1e16fd741c7f0189dd1610e6ec2
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-09-02 07:51:54 +0530

    Time: 6 ms (83.42%), Space: 57.3 MB (39.24%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<Integer> findDuplicates(int[] arr) {
         List<Integer> ans = new ArrayList<>();
-        int n = arr.length;
-        for(int i=0;i<n;i++){
+
+        for(int i=0;i<arr.length;i++){
             int idx = Math.abs(arr[i]);
             if(arr[idx-1] < 0)
             ans.add(idx);
@@ -10,5 +10,8 @@ class Solution {
             arr[idx-1] *= -1;
         }
         return ans;
+
     }
 }
+
+//count sort lagaya hai haar elememt ko -1 se multiple kiya hia agr vo already -1 hai to mtlb duplicate h
